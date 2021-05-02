6dc039e76f9606edf98b253cb71bb2ddbe48b2d9
kfiva <kfivap@gnail.com>
2021-05-02 03:12:52 +0300

    database models

--- server/models/models.js
+++ server/models/models.js
@@ -6,13 +6,35 @@ const User = sequelize.define('user', {
     email: {type: DataTypes.STRING, unique: true},
     nick: {type: DataTypes.STRING, unique: true},
     password: {type: DataTypes.STRING},
-    role: {type: DataTypes.STRING, defaultValue: "ADMIN"},
 })
 
+const Chat = sequelize.define('chat', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    firstUser: {type: DataTypes.INTEGER, allowNull: false},
+    secondUser: {type: DataTypes.INTEGER, allowNull: false}
+})
 
+const Message = sequelize.define('message', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    from: {type: DataTypes.INTEGER, allowNull: false},
+    to: {type: DataTypes.INTEGER, allowNull: false},
+    text: {type: DataTypes.TEXT, allowNull: false},
+    media: {type: DataTypes.TEXT},
+    read: {type: DataTypes.BOOLEAN, defaultValue: false}
+})
 
+const UserChats = sequelize.define('user_chats', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+})
+
+User.belongsToMany(Chat, {through: UserChats})
+Chat.belongsToMany(User, {through: UserChats})
+
+Chat.hasMany(Message)
+Message.belongsTo(Chat)
 
 
 module.exports = {
-User
+    User, Chat, Message,
+    UserChats
 }
